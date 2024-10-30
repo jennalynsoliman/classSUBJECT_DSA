06092712ef3cfffbb4357317635ad04b13f312fb
Jennalyn Soliman <solimanjennalynfuentes@gmail.com>
2024-10-31 07:50:56 +0800

    Add files via upload

---
+++ classSubject.js
@@ -0,0 +1,11 @@
+// classSubjects.js 2
+
+// Collect user information
+const subjectTitle = "Data Structure and Algorithm"; // Example subject title
+const classSchedule = "MF 08:00 AM - 9:00 AM"; // Example class schedule
+const classroom = "Room 124"; // Example classroom
+const classInstructor = "Sir Cliff Owen Pascua"; // Example instructor
+const studentName = "Jennalyn Soliman"; // Example student name
+
+// Store and log the information to the console following the string
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`);
