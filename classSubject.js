a0713e2a9c4353f0f4909dcfa2a3a45b6013a02b
Jennalyn Soliman <solimanjennalynfuentes@gmail.com>
2024-10-31 15:55:19 +0800

    Update classSubject.js

--- classSubject.js
+++ classSubject.js
@@ -1,11 +1,11 @@
 // Problem 2
 
 // Collect user information
-const subjectTitle = "Data Structure and Algorithm"; // Example subject title
-const classSchedule = "MF 08:00 AM - 9:00 AM"; // Example class schedule
+const subjectTitle = "Data Structure and Algorithm"; // subject title
+const classSchedule = "MF 08:00 AM - 9:00 AM"; // class schedule
 const classroom = "Room 124"; // Example classroom
-const classInstructor = "Sir Cliff Owen Pascua"; // Example instructor
-const studentName = "Jennalyn Soliman"; // Example student name
+const classInstructor = "Sir Cliff Owen Pascua"; // instructor
+const studentName = "Jennalyn Soliman"; //student name
 
 // Store and log the information to the console following the string
 console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`);
